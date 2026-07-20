43c427e0fbb3e061787b0bd83e551d6dd552f9c5
EMILE AG <167655849+EMILEAG228@users.noreply.github.com>
2026-07-20 13:42:38 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'agbabli-ai-v3';
+const CACHE_NAME = 'agbabli1-ai-v3';
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(['./', './index.html', './manifest.json']))
